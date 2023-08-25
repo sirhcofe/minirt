551d6c3755d032342215c6f4ba0084877d3ba263
Josh Thor <wunjosh@gmail.com>
2023-08-25 16:59:36 +0800

    feat: get_next_line function added

---
+++ src/get_next_line.c
@@ -0,0 +1,97 @@
+#include "minirt.h"
+
+char	*reset_buffer(char *buffer)
+{
+	char	*reset;
+	int		i;
+	int		j;
+
+	i = 0;
+	while (buffer[i] && buffer[i] != '\n')
+		i++;
+	if (!buffer[i])
+	{
+		free (buffer);
+		return (0);
+	}
+	reset = ft_calloc((ft_strlen(buffer) - i + 1), sizeof(char));
+	i++;
+	j = 0;
+	while (buffer[i])
+		reset[j++] = buffer[i++];
+	free(buffer);
+	buffer = 0;
+	return (reset);
+}
+
+char	*print_line(char *buffer)
+{
+	char	*line;
+	int		i;
+
+	i = 0;
+	if (!buffer[i])
+		return (0);
+	while (buffer[i] && buffer[i] != '\n')
+		i++;
+	line = ft_calloc((i + 2), sizeof(char));
+	i = 0;
+	while (buffer[i] && buffer[i] != '\n')
+	{
+		line[i] = buffer[i];
+		i++;
+	}
+	if (buffer[i] && buffer[i] == '\n')
+		line[i] = '\n';
+	return (line);
+}
+
+char	*join_and_free(char *buffer, char *temp)
+{
+	char	*pepega;
+
+	pepega = ft_strjoin(buffer, temp);
+	free(buffer);
+	buffer = 0;
+	return (pepega);
+}
+
+char	*read_file(char *buffer, int fd)
+{
+	char	*temp;
+	int		byte_read;
+
+	temp = ft_calloc((BUFFER_SIZE + 1), sizeof(char));
+	byte_read = 1;
+	while (!ft_strchr(temp, '\n') && byte_read > 0)
+	{
+		byte_read = read(fd, temp, BUFFER_SIZE);
+		if (byte_read == -1)
+		{
+			free(temp);
+			return (0);
+		}
+		temp[byte_read] = 0;
+		buffer = join_and_free(buffer, temp);
+	}
+	free(temp);
+	temp = 0;
+	return (buffer);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*buffer;
+	char		*line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
+		return (0);
+	if (buffer == NULL)
+		buffer = ft_calloc(1, sizeof(char));
+	buffer = read_file(buffer, fd);
+	if (!buffer)
+		return (0);
+	line = print_line(buffer);
+	buffer = reset_buffer(buffer);
+	return (line);
+}
