5b82e8ace0de469ef63e7a2ad0659d2f3ddad5dc
Josh Thor <wunjosh@gmail.com>
2023-09-21 21:47:58 +0800

    feat: Added new driver functions
    Added get_touchy()
    WIP sroll_obj()
    Added docs

--- includes/minirt.h
+++ includes/minirt.h
@@ -249,13 +249,15 @@ void free_split(char **head);
 */
 void	print_image(t_minirt *rt);
 /**
- * @brief Renders the pixel if the ray that comes from it goes to infinity
- * @return Function does not return.
- * @param rt Pointer to the t_minirt struct that holds everything.
- * @param idx Location of the pixel, to be broken down to the x and y values
- * 
-*/
-void	void_pixel(t_minirt *rt, int idx);
+ * @brief Goes through all shapes (if any) and finds which one is closest to
+ * the camera following the path of the ray passed as a parameter
+ * @return Returns the index of the closest object from the camera using the
+ * path of the ray
+ * @param f_data Pointer to the t_data struct for shortcut purposes
+ * @param ray_vector The directional vector of the ray that originates from
+ * the camera.
+ */
+int		get_touchy(t_data *f_data, t_coord *ray_vector);
 /**
  * @brief Function that prints an empty map, in the absence of objects.
  * @return Function does not return.
@@ -273,6 +275,14 @@ void	empty_protocol(t_minirt *rt);
  * @param colour The intended colour of the pixel.
 */
 void	put_pxl(t_minirt *rt, int x, int y, int colour);
+/**
+ * @brief Renders the pixel if the ray that comes from it goes to infinity
+ * @return Function does not return.
+ * @param rt Pointer to the t_minirt struct that holds everything.
+ * @param idx Location of the pixel, to be broken down to the x and y values
+ * 
+*/
+void	void_pixel(t_minirt *rt, int idx);
 
 /******************************** -.- Utils -.- *******************************/
 
--- src/driver/print_img.c
+++ src/driver/print_img.c
@@ -26,7 +26,7 @@ void	print_image(t_minirt *rt)
 		while (++ctr < (rt->height * rt->width))
 		{
 			ray_vector = get_ray_vector(rt, ctr);
-			index = get_touchy(rt, ray_vector);
+			index = get_touchy(rt->file_data, ray_vector);
 			if (index == -1)
 				void_pixel(rt, ctr);
 			else
@@ -38,6 +38,32 @@ void	print_image(t_minirt *rt)
 		empty_protocol(rt);
 }
 
+double	get_sp_dist(t_coord *ray_vector, t_sp *sphere);
+double	get_pl_dist(t_coord *ray_vector, t_pl *plane);
+double	get_cy_dist(t_coord *ray_vector, t_cy *cylinder);
+
+/**
+ * data[] ;
+ * data[0] -> index to return, should be the index of the closest obj.
+ * data[1] -> index counter, use to keep track of which objet we looking at.
+ * data[2] -> distance between the closest object and the camera.
+ */
+
+int		get_touchy(t_data *f_data, t_coord *ray_vector)
+{
+	double	data[3];
+
+	data[0] = -1;
+	data[1] = 0;
+	if (f_data->num_sp)
+		scroll_obj(&data, ray_vector, f_data->spheres, get_sp_dist);
+	if (f_data->num_pl)
+		scroll_obj(&data, ray_vector, f_data->planes, get_pl_dist);
+	if (f_data->cylinders)
+		scroll_obj(&data, ray_vector, f_data->cylinders, get_cy_dist);
+	return ((int)(data[0]));
+}
+
 void	empty_protocol(t_minirt *rt)
 {
 	size_t	ctr;
@@ -46,4 +72,9 @@ void	empty_protocol(t_minirt *rt)
 	ctr = -1;
 	while (++ctr < (rt->height * rt->width))
 		void_pixel(rt, ctr);
+}
+
+void	scroll_obj(double *data[3], t_coord *r_vect, t_list *lst, double (*f)(t_coord *, void *))
+{
+	
 }
