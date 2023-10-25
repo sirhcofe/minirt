8974a692c12d0af121340b530d9220631337775a
Zer <livefatezz@gmail.com>
2023-10-25 17:22:23 +0800

    feat: calculation of distance between ray and sphere done

--- includes/minirt.h
+++ includes/minirt.h
@@ -284,8 +284,38 @@ void	empty_protocol(t_minirt *rt);
  */
 void	scroll_obj(double *data[3], t_coord *r_vect, t_list *lst, double (*f)(t_coord *, void *));
 
-
+// get_cy_dist.c
+/**
+ * @brief Function determines if an intersection on the cylinder occured on the
+ * ray vector originating from camera origin. In order to ray trace a cylinder,
+ * geometric transformations is required to scale, rotate, and translate the
+ * primitives into desired locations. As a finite unit cylinder equation is a
+ * quadratic equation of second order: x^2 + y^2 = 1, z_min <= z <= z_max. The
+ * equation will give two values of t, and the smallest non-negative value will
+ * be the intersection distance, while the intersection point can be defined by
+ * E + tD
+ * @param dist The distance between the intersect and camera origin
+ * @param ray_vec The ray vector (D) originating from camera origin (E)
+ * @param ray_ori The coordinates of the caemra origin
+ * @param cy The cylinder object
+ * @return Function returns true if an intersection occured; otherwise, returns
+ * false 
+*/
 int	get_cy_dist(double *dist, t_coord ray_vec, t_coord ray_ori, t_cy *cy);
+
+// get_sp_dist.c
+/**
+ * @brief Function determines if an intersection on the sphere occured on the
+ * ray vector. If the discriminant is non-negative, that means that there is
+ * intersection between the ray and the sphere. Solving the quadratic equation
+ * will return either 1 or 2 real values for t, and the smallest, non-negative
+ * value will be the intersection point.
+ * @param dist The distance between the intersect and camera origin
+ * @param ray_vec The ray vector originating from camera origin
+ * @param ray_ori The coordinates of camera origin
+ * @param sp The sphere object
+*/
+int	get_sp_dist(double *dist, t_coord ray_vec, t_coord ray_ori, t_sp *sp);
 
 // draw.c
 /**
--- src/ray_tracing/get_sp_dist.c
+++ src/ray_tracing/get_sp_dist.c
@@ -6,28 +6,49 @@
 /*   By: chenlee <chenlee@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/25 08:15:57 by chenlee           #+#    #+#             */
-/*   Updated: 2023/10/25 11:52:28 by chenlee          ###   ########.fr       */
+/*   Updated: 2023/10/25 17:08:13 by chenlee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 
+double	handle_found(double coeff[3], double discriminant)
+{
+	double	t[2];
+
+	t[0] = (-coeff[1] + sqrt(discriminant)) / (2 * coeff[0]);
+	t[1] = (-coeff[1] - sqrt(discriminant)) / (2 * coeff[0]);
+	if (t[0] >= 0 && t[1] >= 0)
+	{
+		if (t[0] < t[1])
+			return (t[0]);
+		else
+			return (t[1]);
+	}
+	else
+	{
+		if (t[0] >= 0)
+			return (t[0]);
+		else
+			return (t[1]);
+	}
+}
+
 int	get_sp_dist(double *dist, t_coord ray_vec, t_coord ray_ori, t_sp *sp)
 {
 	double	coeff[3];
+	double	discriminant;
+	t_coord	sphere_offset;
 
-	coeff[0] = pow(ray_vec.x, 2) + pow(ray_vec.y, 2) + pow(ray_vec.z, 2);
+	sphere_offset = vect_subt(ray_ori, sp->center);
+	coeff[0] = dot_prod(ray_vec, ray_vec);
+	coeff[1] = 2 * dot_prod(ray_vec, sphere_offset);
+	coeff[2] = dot_prod(sphere_offset, sphere_offset) - 1;
+	discriminant = pow(coeff[1], 2) - 4 * coeff[0] * coeff[2];
+	if (discriminant >= 0)
+	{
+		*dist = handle_found(coeff, discriminant);
+		return (1);
+	}
+	return (0);
 }
-
-/**
- * @brief Function determines if an intersection on the plane occured on the ray
- * vector. If the denominator (in which is the dot product of the plane's normal
- * vector and ray vector) is 0, then the ray is parallel to the plane, and there
- * is no intersection. If the intersetion t < 0, the plane is behind the camera
- * and there is no intersection; otherwise the intersection point is E + tD.
- * 
- * @param dist The distance between the intersect and camera origin
- * @param ray_vec The ray vector (D) originating from camera origin (E)
- * @param ray_ori The coordinates of camera origin
- * @param pl The plane object
-*/
