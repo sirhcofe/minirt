51896df30ceb67c76e06427f4b55b413df917db6
Josh Thor <wunjosh@gmail.com>
2023-11-28 18:18:39 +0800

    chore: Removed test cases

--- src/main2.c
+++ src/main2.c
@@ -20,9 +20,7 @@ int main(int argc, char **argv)
 	arg_check(argc, argv);
 	rt = init_mlx_window();
 	rt->file_data = parse_file(argv[1]);
-	printf("DONE\n");
 	print_image(rt);
-	printf("DONE2\n");
 	mlx_put_image_to_window(rt->mlx, rt->mlx_win, rt->img, 0, 0);
 	set_controls(rt);
 	mlx_loop(rt->mlx);
--- src/ray_tracing/render_utils.c
+++ src/ray_tracing/render_utils.c
@@ -49,17 +49,15 @@ t_coord	deduce_normal(t_coord p2p, t_coord normal)
 	return (normal);
 }
 
-t_coord	get_cy_normal(t_cy obj, t_coord intsct)
+t_coord    get_cy_normal(t_cy obj, t_coord intsct)
 {
-	double	dist;
-	t_coord	cross_res;
-	t_coord	p2p_vector;
+    double    dist;
+    t_coord    p2p_vector;
+    t_coord    projection;
+    t_coord    subtract_projection;
 
-	p2p_vector = vect_subt(intsct, obj.center);
-	cross_res = cross_prod(obj.axis_vector, p2p_vector);
-	dist = vect_magnitude(cross_res, (t_coord){0, 0, 0, 1});
-	if (approx(dist, obj.radius) == 1) // The intsct point is on the curved surface
-		return (deduce_normal(normalize(p2p_vector), normalize(cross_res)));
-	else
-		return (deduce_normal(normalize(p2p_vector), obj.axis_vector));
+    p2p_vector = vect_subt(intsct, obj.center);
+    projection = vect_mult(obj.axis_vector, dot_prod(projection, obj.axis_vector) / pow(vect_magnitude(intsct, obj.center), 2));
+    subtract_projection = normalize(vect_subt(p2p_vector, projection));
+    return (subtract_projection);
 }
