e9d01514803d3c371928e33eae352424c32881a1
Zer <livefatezz@gmail.com>
2024-01-11 17:25:54 +0800

    fix: phong should be working now

--- src/ray_tracing/render_pixel3.c
+++ src/ray_tracing/render_pixel3.c
@@ -117,13 +117,14 @@ t_coord	reflect(t_coord i, t_coord n)
 	return (res);
 }
 
+// 2-10 for matte sueface; 10-200 for moderate shiny; 1000++ for polished metal/mirror
 t_rgb	specular_component(t_minirt *rt, t_coord reflect_dir, t_coord to_viewer)
 {
 	double	spec;
 	double	shininess;
 	t_rgb	specular;
 
-	shininess = 20;
+	shininess = 5;
 	spec = pow(fmax(dot_prod(reflect_dir, to_viewer), 0.0), shininess);
 	specular.red = spec * rt->file_data->light.colour.red
 			* rt->file_data->light.ratio;
@@ -163,9 +164,64 @@ t_rgb	ambient_component(t_minirt *rt, t_object *obj, t_rgb obj_color)
 	return (ambient);
 }
 
-t_rgb	phong(t_minirt *rt, t_object *obj, t_rgb obj_color, t_coord intersect)
+enum {
+	to_light = 0,
+	to_viewer = 1,
+	normal = 2,
+	reflect_dir = 3,
+};
+
+t_rgb	phong(t_minirt *rt, t_object *obj, t_rgb obj_color, t_coord *vectors)
 {
-	
+	t_rgb	ambient;
+	t_rgb	diffuse;
+	t_rgb	specular;
+
+	ambient = ambient_component(rt, obj, obj_color);
+	diffuse = diffuse_component(rt, vectors[normal], vectors[to_light], obj_color);
+	specular = specular_component(rt, vectors[reflect_dir], vectors[to_viewer]);
+	return ((t_rgb){
+		clamp(ambient.red + diffuse.red + specular.red, 0.0, 1.0),
+		clamp(ambient.green + diffuse.green + specular.green, 0.0, 1.0),
+		clamp(ambient.blue + diffuse.blue + specular.blue, 0.0, 1.0)
+	});
+}
+
+int	ft_inshadow(t_data *f_data, t_coord intsct_pt, int index, t_coord to_light)
+{
+	t_list	*lst;
+	int		curr;
+	double	dist_val;
+
+	curr = 0;
+	lst = f_data->objects;
+	while (lst)
+	{
+		if (index != curr)
+		{
+			dist_val = get_curr_dist(to_light, intsct_pt, lst->content);
+			if (dist_val != INFINITY) // there is an object in the way
+			{
+				if (dist_val <= vect_magnitude(intsct_pt, f_data->light.point))
+					return (1);
+				// else the "obstructing" object is behind the light, so technically not in shadow
+			}
+		}
+		curr++;
+		lst = lst->next;
+	}
+	return (0);
+}
+
+void	calc_vect(t_minirt *rt, t_object *obj, t_coord *vectors, t_coord intsct)
+{
+	vectors[to_light] = normalize(vect_subt(rt->file_data->light.point,
+			intsct));
+	vectors[to_viewer] = normalize(vect_subt(rt->file_data->camera.point,
+			intsct));
+	vectors[normal] = get_normal(obj, intsct, vectors[to_light]);
+	vectors[reflect_dir] = reflect(vect_mult(vectors[to_light], -1.0),
+			vectors[normal]);
 }
 
 void	render_pixel(t_minirt *rt, int index, size_t ctr)
@@ -173,49 +229,24 @@ void	render_pixel(t_minirt *rt, int index, size_t ctr)
 	t_object	*obj;
 	t_rgb		obj_color;
 	t_coord		intersect_pt;
-	t_coord		to_light;
-	t_coord		to_viewer;
-	t_coord		normal;
-	t_coord		reflect_dir;
-	t_rgb		light_color;
-	double		light_intensity;
-	light_color = rt->file_data->light.colour;
-	light_intensity = rt->file_data->light.ratio;
-	int			shininess = 20; // 2-10 for matte sueface; 10-200 for moderate shiny; 1000++ for polished metal/mirror
+	t_coord		vectors[4];
+	t_rgb		final;
 
 	obj = get_object(rt->file_data->objects, index);
 	obj_color = get_colour(obj);
 	intersect_pt = get_intsct_point(obj);
-	to_light = normalize(vect_subt(rt->file_data->light.point, intersect_pt));
-	to_viewer = normalize(vect_subt(rt->file_data->camera.point, intersect_pt));
-	normal = get_normal(obj, intersect_pt, to_light);
-	reflect_dir = reflect(to_light, normal);
-
-	// ambient component
-	t_rgb	ambient;
-	ambient = ambient_component(rt, obj, obj_color);
-	
-
-	// diffuse component
-	t_rgb	diffuse;
-	diffuse = diffuse_component(rt, normal, to_light, obj_color);
-
-	// specular component
-	t_rgb	specular;
-	specular = specular_component(rt, reflect_dir, to_viewer);
-
-	// combine component
-	t_rgb	final;
-	final = phong(rt, obj, obj_color, intersect_pt);
-
-	final.red = ambient.red + diffuse.red + specular.red;
-	final.green = ambient.green + diffuse.green + specular.green;
-	final.blue = ambient.blue + diffuse.blue + specular.blue;
-
-	// clamp
-	final.red = clamp(final.red, 0.0, 1.0);
-	final.green = clamp(final.green, 0.0, 1.0);
-	final.blue = clamp(final.blue, 0.0, 1.0);
+	calc_vect(rt, obj, vectors, intersect_pt);
+	if (ft_inshadow(rt->file_data, intersect_pt, index, vectors[to_light])) // there is an object in the way
+	{
+		final.red = obj_color.red * rt->file_data->ambience.colour.red
+				* rt->file_data->ambience.ratio;
+		final.green = obj_color.green * rt->file_data->ambience.colour.green
+				* rt->file_data->ambience.ratio;
+		final.blue = obj_color.blue * rt->file_data->ambience.colour.blue
+				* rt->file_data->ambience.ratio;
+	}
+	else
+		final = phong(rt, obj, obj_color, vectors);
 
 	// blend
 	double	pxl_col = create_colour(0, final.red * 255, final.green * 255, final.blue * 255);
